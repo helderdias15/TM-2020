32b3a145f9e9b1e31d182daeffda1948021b6f91
helderdias15 <61152845+helderdias15@users.noreply.github.com>
2020-03-09 15:00:52 +0000

    HelderGomes-22054

---
+++ Aula 2/exercicio2/HelderGomes-22054/script.js
@@ -0,0 +1,15 @@
+var arrayNumbers=[];
+
+function adicionar() {
+    var number = document.getElementById("newNumbers").value;
+    document.getElementById("newNumbers").value = "";
+    arrayNumbers.push(parseInt(number));
+    console.log(arrayNumbers)
+
+}
+
+function calcular() {
+    alert(Math.max.apply(Math,arrayNumbers)
+
+
+}
