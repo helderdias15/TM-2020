600718e80820de4ae8c6b7f743960558c0206f50
helderdias15 <61152845+helderdias15@users.noreply.github.com>
2020-03-30 14:21:01 +0100

    HelderGomes-22054

---
+++ Aula 5/PongGame-HelderGomes22054/Assets/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Manager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Score 01")]
+    public GameObject score;
+
+    private int playerScore;
+
+    public void Player1Scored()
+    {
+        playerScore++;
+        score.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+        ball.GetComponent<Ball>().IncreaseSpeed();
+
+    }
+
+    public void Reset()
+    {
+        playerScore = 0;
+        score.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+    }
+
+}
